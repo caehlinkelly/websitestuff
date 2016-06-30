10e62d3e074908664070b80e29bc915a98a5eda1
Caehlin Kelly <cekelly2@uncg.edu>
2016-06-30 16:50:49 -0400

    Add files via upload

---
+++ www/index.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+	<title>Caehlin Kelly | Home</title>
+    <link rel="stylesheet" type="text/css" href="css.css">
+</head>
+
+<body>
+<header>
+    
+    <div id="title stuff">
+        <h1><center>Caehlin Kelly</center></h1>
+        <h3><center><a href="mailto:caehlinkelly@gmail.com">Email</a> || <a href="https://github.com/caehlinkelly">Github</a></center></h3>
+    </div>
+    </header>
+    <hr>
+    <br>
+  
+    <br>
+  <div id="wrap">
+        <center><h2>Who am i?</h2></center>
+        <p> I am a self-taught programmer, avid gamer and web developer. I am currently a Pre-engineering Computer Science student at University of North Carolina at Greensboro. I plan to transfer to North Carolina State University after two years to finish my Computer Science degree. I currently live in Holly Springs, North Carolina, and aspire to be a Software Developer and possibly work in Video Game Development.</p>
+    </div>
+            <img source="/images/update.jpeg" height="100" width=auto>
+    <div id ="menu">
+        <center><h2>What can I do?</h2></center>
+        <p>I dabble in:</p>
+        <ul>
+            <li><a href="../pages/java.html">Java</a></li>
+            <li><a href="../pages/csharp.html">C#</a></li>
+            <li>C++</li>
+            <li><a href="../pages/python.html">Python</a></li>
+            <li>Visual Basic (Not showcased)</li>
+            <li>MySQL</li>
+            <li>HTML/CSS</li>
+            <li>Graphic Design</li>
+        </ul>
+    </div>
+
+    <footer>
+        <h5>Design by Caehlin Kelly 2016</h5>
+    </footer>
+
+</body>
+
+</html>
---
+++ www/pages/java.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+	<title>Caehlin Kelly | Java</title>
+    <link rel="stylesheet" type="text/css" href="css.css">
+</head>
+
+<body>
+<header>
+    
+    <div id="title stuff">
+        <h1><center>Caehlin Kelly</center></h1>
+        <h3><center><a href="mailto:caehlinkelly@gmail.com">Email</a> | <a href="https://github.com/caehlinkelly">Github</a></center></h3>
+    </div>
+    </header>
+    <hr>
+    <br>
+    <div id="wrap">
+        <center><h2>Java</h2></center>
+        <p>Java is the first language I learned and by far my favorite. I was originally self-taught, but prospered from taking AP Computer Science my junior year of high school. From that moment forward, I knew programming was my destiny.</p>
+        <h4>Quick Facts</h4>
+        <ul>
+            <li>Been using since: 2014</li>
+            <li>Used for: FIRST Robotics and APCS</li>
+            <li>Rate: 5/5</li>
+        </ul>
+        <center><a href="https://github.com/caehlinkelly/javaprograms">Check out some of my programs here!</a></center>
+    </div>
+    <br>
+    <footer>
+<h5>Design by Caehlin Kelly 2016|</h5>
+        <h6>
+ <!-- BEGIN: Powered by Supercounters.com -->
+<script type="text/javascript" src="http://widget.supercounters.com/online_t.js"></script><script type="text/javascript">sc_online_t(1041665,"Viewing my work","#83afd6");</script><noscript><a href="http://www.supercounters.com/">User Online</a></noscript>
+<!-- END: Powered by Supercounters.com --> | <!-- BEGIN: Powered by Supercounters.com -->
+<script type="text/javascript" src="http://widget.supercounters.com/texthit.js"></script><script type="text/javascript">var sc_texthit_var = sc_texthit_var || [];sc_text_hit(1041667,"Viewed","93BfE7");</script><noscript><a href="http://www.supercounters.com">Users Visted</a></noscript>
+<!-- END: Powered by Supercounters.com -->
+        </h6>
+    </footer>
+    </body>
+</html>
